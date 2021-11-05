b96ed411f060231191f5f7ef6d9b11d2485d2ca5
Andrey Eroshkin <andreroshkin@gmail.com>
2021-11-05 18:36:22 +0300

    reverseList

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let current = head;
+    while (current) {
+        let tmp = current.next;
+        // reverse pointer
+        current.next = prev;
+        // step forward in the list
+        prev = current;
+        current = tmp;
+    }
+    return prev;
+};
+// @lc code=end
+
