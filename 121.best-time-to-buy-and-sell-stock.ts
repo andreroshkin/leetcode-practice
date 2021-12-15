61891ece6f130d40cf01f2c85ccaf7809032d74e
Andrey Eroshkin <andreroshkin@gmail.com>
2021-12-15 22:51:17 +0300

    maxProfit

---
+++ 121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=121 lang=typescript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+    let maxProfit: number = 0;
+    let currentMin: number = prices[0]; 
+    for (const price of prices) {
+        maxProfit = Math.max(maxProfit, price - currentMin);
+        currentMin = Math.min(currentMin, price)
+    }
+    return maxProfit;
+};
+// @lc code=end
+
