1715724261ad85d365d0893903bccec559c1cb64
Andrey Eroshkin <andreroshkin@gmail.com>
2021-12-22 22:43:17 +0300

    binary search

---
+++ 704.binary-search.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+    let start: number = 0;
+    let end: number = nums.length - 1;
+
+    while (start <= end) {
+        let middle: number = Math.round((start + end) / 2);
+        if (nums[middle] === target) {
+            return middle;
+        } else if (nums[middle] < target) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
+    return -1;
+};
+// @lc code=end
+
