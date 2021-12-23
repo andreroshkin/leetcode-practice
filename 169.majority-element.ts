98232705acb9c870dc67bac825e915a24540c4f0
Andrey Eroshkin <andreroshkin@gmail.com>
2021-12-23 20:25:25 +0300

    majorityElement

---
+++ 169.majority-element.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=169 lang=typescript
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+//Boyer-Moore Majority Voting Algorithm
+function majorityElement(nums: number[]): number {
+    let counter: number = 0;
+    let candidate: number = -1;
+    for (const num of nums) {
+        if (counter === 0) {
+            candidate = num;
+            counter = 1;
+        } else {
+            if (candidate === num) {
+                counter++
+            } else {
+                counter--;
+            }
+        }
+    }
+
+    for (const num of nums) {
+        if (num === candidate) {
+            counter++;
+        }
+    }
+    if (counter > (nums.length / 2)) {
+        return candidate;
+    } else {
+        return -1;
+    }
+};
+// @lc code=end
+
