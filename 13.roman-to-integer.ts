2681865dcbb8cf8d1de5dc2e910ee1ec1b1bb0c4
Andrey Eroshkin <andreroshkin@gmail.com>
2021-12-24 20:56:57 +0300

    romanToInt

---
+++ 13.roman-to-integer.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=13 lang=typescript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+function romanToInt(s: string): number {
+    enum Roman {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000,
+    };
+
+    let result: number = 0;
+
+    for (let index = 0; index < s.length; index++) {
+        const current: number = Roman[s[index]];
+        const next: number = Roman[s[index + 1]];
+
+        if (current < next) {
+            result += next - current;
+            index++;
+        } else {
+            result += current;
+        }
+    }
+
+    return result;
+}
+// @lc code=end
