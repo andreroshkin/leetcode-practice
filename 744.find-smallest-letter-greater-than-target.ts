a3dc43c7bd67455e3bb06255a587655916709696
Andrey Eroshkin <andreroshkin@gmail.com>
2022-04-03 11:46:37 +0300

    nextGreatestLetter

---
+++ 744.find-smallest-letter-greater-than-target.ts
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=744 lang=typescript
+ *
+ * [744] Find Smallest Letter Greater Than Target
+ */
+
+// @lc code=start
+function nextGreatestLetter(letters: string[], target: string): string {
+    const indexGreatestLetter: number = letters.findIndex((letter) => letter > target);
+    return letters[indexGreatestLetter === -1 ? 0 :  indexGreatestLetter]
+};
+// @lc code=end
+
