97812360ff77b312d72427963e208105205f20c7
Andrey Eroshkin <andreroshkin@gmail.com>
2022-04-11 18:53:31 +0300

    Invert Binary Tree

---
+++ 226.invert-binary-tree.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=226 lang=typescript
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(node: TreeNode | null): TreeNode | null {
+    if (!node) return null;
+
+    const left = invertTree(node.left);
+    const right = invertTree(node.right);
+
+    node.left = right;
+    node.right = left;
+
+    return node;
+}
+// @lc code=end
