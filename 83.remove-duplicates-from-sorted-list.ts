72519bc7fc1b8f0da4415ab2896a4f541578271a
Andrey Eroshkin <andreroshkin@gmail.com>
2022-04-12 18:47:43 +0300

    Remove Duplicates from Sorted List

---
+++ 83.remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=83 lang=typescript
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let current = head;
+
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+}
+// @lc code=end
