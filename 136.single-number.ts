7bf245db76e2695acfbd7e6dca30c2da9a37e78d
Andrey Eroshkin <andreroshkin@gmail.com>
2022-04-12 18:33:05 +0300

    Single Number

--- 136.single-number.ts
+++ 136.single-number.ts
@@ -1,20 +1,15 @@
 /*
- * @lc app=leetcode id=136 lang=javascript
+ * @lc app=leetcode id=136 lang=typescript
  *
  * [136] Single Number
  */
 
 // @lc code=start
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var singleNumber = function(nums) {
+function singleNumber(nums: number[]): number {
     let x = 0;
     for (const num of nums) {
         x = x ^ num;
     }
     return x;
-};
+}
 // @lc code=end
-
