837b5a26687250bd2bc4b42321cd7c4f43f468e6
Andrey Eroshkin <andreroshkin@gmail.com>
2022-05-26 23:15:57 +0300

    Generate Parentheses

---
+++ 22.generate-parentheses.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=22 lang=typescript
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+
+function generate(result: string[], s: string, open: number, close: number, n: number) {
+    if (open === n && close === n) {
+        result.push(s);
+        return;
+    }
+
+    if (open < n) {
+        generate(result, s + '(', open + 1, close, n);
+    }
+    if (close < open) {
+        generate(result, s + ')', open, close + 1, n);
+    }
+}
+
+function generateParenthesis(n: number): string[] {
+    const result = [];
+    generate(result, '', 0, 0, n);
+    return result;
+}
+// @lc code=end
