f018eed11ea12624d77166bed3cd2b7386dc8755
Andrey Eroshkin <andreroshkin@gmail.com>
2025-09-06 16:20:56 +0300

    twoSum

--- 1.two-sum.ts
+++ 1.two-sum.ts
@@ -5,16 +5,29 @@
  */
 
 // @lc code=start
+/**
+ * Находит два числа в массиве, сумма которых равна целевому значению
+ * @param nums - массив целых чисел
+ * @param target - целевая сумма
+ * @returns массив из двух индексов чисел, которые дают целевую сумму
+ */
 function twoSum(nums: number[], target: number): number[] {
+    // Map для хранения пар "число -> индекс" уже просмотренных элементов
     const indexes = new Map();
 
+    // Проходим по массиву один раз
     for (let index = 0; index < nums.length; index++) {
         const currentNum = nums[index];
+        // Вычисляем число, которое нужно найти для получения целевой суммы
         const complement = target - currentNum;
 
+        // Проверяем, встречали ли мы ранее нужное дополняющее число
         if (indexes.has(complement)) {
+            // Если да, возвращаем индекс найденного числа и текущий индекс
             return [indexes.get(complement), index];
         }
+
+        // Сохраняем текущее число и его индекс для будущих проверок
         indexes.set(nums[index], index);
     }
 }
