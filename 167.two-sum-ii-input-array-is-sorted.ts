2adea919d460f1e07a34e19fe5dedc8a458ba284
Andrey Eroshkin <andreroshkin@gmail.com>
2026-01-08 17:27:28 +0300

    feat(master): twoSum

---
+++ 167.two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=167 lang=typescript
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+function twoSum(numbers: number[], target: number): number[] {
+    let left = 0;
+    let right = numbers[numbers.length - 1];
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return [];
+}
+// @lc code=end
