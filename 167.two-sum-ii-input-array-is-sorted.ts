cc70ea605ac9382cd7c380920640545bc3c7509f
Andrey Eroshkin <andreroshkin@gmail.com>
2026-01-08 17:29:15 +0300

    feat(master): twoSum

--- 167.two-sum-ii-input-array-is-sorted.ts
+++ 167.two-sum-ii-input-array-is-sorted.ts
@@ -7,7 +7,7 @@
 // @lc code=start
 function twoSum(numbers: number[], target: number): number[] {
     let left = 0;
-    let right = numbers[numbers.length - 1];
+    let right = numbers.length - 1;
 
     while (left < right) {
         const sum = numbers[left] + numbers[right];
